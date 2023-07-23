053e66375a8fd39292f4e41012bc8fc7d883d81e
sahanruwanthasilva <sahanr.silva@hotmail.com>
2023-07-23 08:25:07 +0530

    configured slack integration

--- app/page.tsx
+++ app/page.tsx
@@ -2,39 +2,38 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-      <main>
-          <div className="bg-white">
-              <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                  <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-                      Customers also purchased
-                  </h2>
+      <div>
+          <header className="bg-orange-500 text-white py-4 text-center">
+              <h1 className="text-3xl">MicroMarket: Revolutionizing Small Business Online Presence</h1>
+          </header>
+          <main className="max-w-3xl mx-auto mt-4 p-4 bg-white rounded shadow-md">
+              <h2 className="text-teal-500 mt-4">Key Points</h2>
+              <ul className="list-disc pl-4">
+                  <li>Traditional marketplaces often limit businesses to showcasing products only, but MicroMarket goes beyond that by providing a full-fledged marketplace experience.</li>
+                  <li>Small businesses no longer need to create individual websites; MicroMarket eliminates the need for complex website development.</li>
+                  <li>With MicroMarket, entrepreneurs can effortlessly showcase and manage their products, reaching a wider audience and expanding their brand presence.</li>
+              </ul>
 
-                  <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                      <div className="group relative">
-                          <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                              <img
-                                  src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
-                                  alt="Front of men's Basic Tee in black."
-                                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                              />
-                          </div>
-                          <div className="mt-4 flex justify-between">
-                              <div>
-                                  <h3 className="text-sm text-gray-700">
-                                      <a href="#">
-                                          <span aria-hidden="true" className="absolute inset-0"></span>
-                                          Basic Tee
-                                      </a>
-                                  </h3>
-                                  <p className="mt-1 text-sm text-gray-500">Black</p>
-                              </div>
-                              <p className="text-sm font-medium text-gray-900">$35</p>
-                          </div>
-                      </div>
-                      {/* Add more related products here */}
-                  </div>
+              <h2 className="text-teal-500 mt-4">Benefits</h2>
+              <ul className="list-disc pl-4">
+                  <li>Comprehensive solution for small business online presence.</li>
+                  <li>Enhanced customer engagement through advanced marketplace functionality.</li>
+                  <li>Streamlined product management and customization options.</li>
+                  <li>Integrated marketing tools for increased brand visibility.</li>
+              </ul>
+
+              <h2 className="text-teal-500 mt-4">Cutting-Edge Technology</h2>
+              <ul className="list-disc pl-4">
+                  <li><strong>Backend:</strong> Our backend is built using Spring Boot, a powerful and flexible framework that provides a robust foundation for microservices architecture. This enables seamless scalability, fault tolerance, and efficient handling of business logic.</li>
+                  <li><strong>Frontend:</strong> The frontend is crafted using Next.js, a popular React framework that offers server-side rendering, enhanced performance, and a delightful user experience. With Next.js, we ensure a responsive and dynamic interface for your customers.</li>
+              </ul>
+
+              <h2 className="text-teal-500 mt-4">Join MicroMarket Today!</h2>
+              <p>Embrace MicroMarket and embark on a journey of growth and success! Our platform empowers small businesses to thrive in the digital landscape. Say goodbye to limitations and unleash your full potential with MicroMarket.</p>
+              <div className="text-center">
+                  <img src="path/to/logo.png" alt="MicroMarket Logo" className="block max-w-full mx-auto rounded shadow-md" style={{ maxWidth: '200px' }} />
               </div>
-          </div>
-      </main>
+          </main>
+      </div>
   )
 }
